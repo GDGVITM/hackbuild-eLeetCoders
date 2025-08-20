5817ea8ed7e21d1f8e87cba0d2a52aad96f3e94a
AryanRacha <aryanracha@gmail.com>
2025-08-20 21:06:55 +0530

    Initail Commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Frontend
+
+React.js, TypeScript, Tailwind
+React-Router, ShadCN, Zustand
+
+# Backend
+
+Node.js, Express.js, TypeScript
+Firebase, Routes, DB Connection
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen text-red-600 flex items-center justify-center text-3xl text-center">
+        Hello World!
+      </div>
+    </>
+  );
+}
+
+export default App;
